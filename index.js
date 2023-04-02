0649b8e595995ecb4d42ae00078cbd2b66806df7
Nelly Putpana <nelly.putpana@gmail.com>
2023-04-01 20:11:06 -0600

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2
+console.log(multiply);
+
+const random = 1
+Number.isInteger(random);
+
+const num3 = 8
+const num4 = 2
+const mod = num3 / num4
+console.log(mod);
+
+const max = 20;
+Math.max(5,10,15,20);
